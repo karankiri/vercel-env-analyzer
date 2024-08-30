1a24aca7b5ce3862041f6895a61b9ca28b07bfd3
Karan Kiri <karankiri.96@gmail.com>
2024-08-30 14:11:11 +0530

    initial changes

--- src/content.ts
+++ src/content.ts
@@ -7,11 +7,14 @@ interface EnvVariable {
 function crawlVercelPage(): EnvVariable[] {
   const variables: EnvVariable[] = [];
   const rows = document.querySelectorAll('[class^="env-variables-table_resultRow"]');
+  console.log("🚀 ~ crawlVercelPage ~ rows:", rows)
 
   rows.forEach((row) => {
     const name = row.querySelector('[class^="env-variables-table_varName"]')?.textContent?.trim() || '';
+    console.log("🚀 ~ rows.forEach ~ name:", name)
     const environments = row.querySelector('[class^="entity_description"]')?.textContent?.trim()?.split(", ")
       .filter(env => env !== '');
+    console.log("🚀 ~ rows.forEach ~ environments:", environments)
 
     variables.push({ name, environments: environments ?? [] });
   });
